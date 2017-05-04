5f501ef0130f937a5a3a2919c692b673cf6dbde8
Xiaofei <xiaofei.zhang@uwaterloo.ca>
2017-05-04 17:00:09 -0400

    add timing for rabbit

--- main.c
+++ main.c
@@ -10,7 +10,18 @@
 int main(int argc, char ** argv)
 {
 
-	printf("Rabbit hopping ...\n");
+
+	printf("Rabbit gets ready ...\n");
+
+	printf("Rabbit starts hopping ...\n");
+
+	clock_t begin = clock();
+
+	clock_t end = clock();
+
+	float t_total = ((float)(end-begin)/1000000.0F)/1000;
+
+	printf("Rabbit hopping for %f ms, exhausted\n", t_total);
 
 	return 0;
 }
